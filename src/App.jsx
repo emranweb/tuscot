b6d21fe20357bebca34f51d3e6fd4fde51e06944
Emran <emranbd77@gmail.com>
2023-03-14 19:05:19 +0600

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Page404 from "./pages/404";
+import Home, { action as rootAction, loader as rootLoader } from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import ContactDetails, {
+  loader as contactLoader,
+} from "./pages/ContactDetails";
+import EditContact, { action as editAction } from "./pages/Edit";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <ContactDetails />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="*" element={<Page404 />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
