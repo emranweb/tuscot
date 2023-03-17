c40c64a78f76b4c1f22415260ea89271574e1eaa
emranweb <emranbd77@gmail.com>
2023-03-17 17:00:09 +0600

    user authentication

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import * as dotenv from "dotenv";
 
 // user sign in controller
 const userSignIn = (req, res) => {
@@ -9,7 +11,20 @@ const userSignIn = (req, res) => {
 
 const userSignUp = (req, res) => {
     const { name, email, password } = req.body;
-    const user = { name, email, password: password };
+    let token = jwt.sign(data, process.env.JSON_WEB_TOKEN_KEY);
+    bcrypt
+        .hash(password, Number(process.env.PASSWORD_ENCRIPT_SALT_ROUND))
+        .then((data) => {
+            const user = {
+                name: name,
+                email: email,
+                password: data,
+                token: token,
+            };
+            console.log(user);
+        });
+
+    res.send("hi");
 };
 
 const userController = { userSignIn, userSignUp };
